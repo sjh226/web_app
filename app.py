a837a595d0d3212ab04e7a9d8061c8bee085cb9c
Steve Harshman <sharshman193@gmail.com>
2017-05-30 15:11:24 -0600

    new port

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8105, threading=True)
+    app.run(host='0.0.0.0', port=8105, threaded=True)
